515e9b547675e7ea2b001321bec6db968ddc27ba
Ryan Salcido <rsalcido@goraft.tech>
2021-05-18 17:49:24 -0700

    Fix lint errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
+import { Fragment } from "react";
+import Head from "next/head";
 import { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
-    <Component {...pageProps} />
+    <Fragment>
+      <Head>
+        <title>Chat Application with Next.js</title>
+      </Head>
+      <Component {...pageProps} />
+    </Fragment>
   );
 };
 
