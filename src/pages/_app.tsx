c10e0b9f6ae585444e869b9997a64a1b844a5258
Ryan Salcido <rsalcido@goraft.tech>
2021-05-31 17:49:01 -0700

    Add GitHub authentication using 'next-auth'

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { Fragment } from "react";
 import Head from "next/head";
 import { AppProps } from "next/app";
+import { Provider } from "next-auth/client";
 import "tailwindcss/tailwind.css";
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
@@ -10,7 +11,9 @@ const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
         <title>Chat Application with Next.js</title>
         <link rel="icon" href={`${process.env.BASE_PATH}/favicon.ico`} />
       </Head>
-      <Component {...pageProps} />
+      <Provider session={pageProps.session}>
+        <Component {...pageProps} />
+      </Provider>
     </Fragment>
   );
 };
