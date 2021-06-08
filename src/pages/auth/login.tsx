d87c0fc0803dbff725771197a624755219671cc3
Ryan Salcido <rsalcido@goraft.tech>
2021-06-07 18:35:22 -0700

    Update animation and fix import path for public resources

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -46,7 +46,7 @@ const Login = (): JSX.Element => {
       <h1 className="text-5xl pb-6 text-black dark:text-white">Log in to Chat</h1>
 
       {error && (
-        <div className="text-white bg-purple-600 rounded mb-8 p-2 text-center w-80 origin-top animate-bounce-4 duration-200">
+        <div className="text-white bg-purple-600 rounded mb-7 p-2 text-center w-80 origin-top animate-pulse-3">
           <p>{error}</p>
         </div>
       )}
@@ -56,7 +56,7 @@ const Login = (): JSX.Element => {
           className="flex text-lg items-center space-x-3 w-full bg-[#333333] text-white p-3 rounded hover:bg-gray-600 shadow-md focus:outline-none transition ease-out duration-500"
           onClick={() => signIn("github")}
         >
-          <Image src="/github.svg" width={26} height={26} />
+          <Image src={`${process.env.BASE_PATH}/github.svg`} width={26} height={26} />
           <span>
             Log in with GitHub
           </span>
@@ -66,7 +66,7 @@ const Login = (): JSX.Element => {
           className="flex text-lg items-center space-x-3 w-full bg-blue-400 text-white p-3 rounded hover:bg-blue-500 shadow-md focus:outline-none transition ease-out duration-500"
           onClick={() => signIn("twitter")}
         >
-          <Image src="/twitter.svg" width={26} height={26} />
+          <Image src={`${process.env.BASE_PATH}/twitter.svg`} width={26} height={26} />
           <span>
             Log in with Twitter
           </span>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,7 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        "bounce-4": "bounce 1s 4"
-      }
-    },
-    keyframes: {
-      bounce: {
-        "0%, 100%": {
-          transform: "none",
-          "animation-timing-function": "cubic-bezier(0,0,0.2,1)"
-        },
-        "50%": {
-          transform: "translateY(-20%)",
-          "animation-timing-function": "cubic-bezier(0.8,0,1,1)"
-        }
+        "pulse-3": "pulse 2s cubic-bezier(0.4, 0, 0.6, 1) 3"
       }
     }
   },
